47dde10c0ecee38408a8c68b8311c24455dbdbec
Yusuf Abdulwahab <yoosif@windowslive.com>
2021-12-07 21:57:42 +0300

    fixing errors

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ app = FastAPI()
 app.include_router(routers.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.0", port=9000)
+    uvicorn.run(app, host="127.0.0.1", port=9000)
 
